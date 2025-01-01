2676612ce6e12002c9e59e1507b35f83f8234ff9
bw-green <bwgreen621@gmail.com>
2025-01-01 22:48:23 +0900

    네비게이션 바 구현

---
+++ app/src/main/java/com/example/kuit_4th_hackathon_team2_android/reservation/ReservationData.kt
@@ -0,0 +1,8 @@
+package com.example.kuit_4th_hackathon_team2_android.reservation
+
+data class ReservationData(
+    val producrId : String,
+    val productName : String,
+    val remainNumber : Int,
+    val rentalPeriod : Int,
+)
