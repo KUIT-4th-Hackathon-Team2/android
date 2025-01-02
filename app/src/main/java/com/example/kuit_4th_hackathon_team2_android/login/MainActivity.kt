e923e73f76448d5c28e4aa6b2ad9b349d9f61d0a
bw-green <102360569+bw-green@users.noreply.github.com>
2025-01-02 18:03:51 +0900

    login 화면이랑 myPage 구현

--- app/src/main/java/com/example/kuit_4th_hackathon_team2_android/login/MainActivity.kt
+++ app/src/main/java/com/example/kuit_4th_hackathon_team2_android/login/MainActivity.kt
@@ -2,12 +2,10 @@ package com.example.kuit_4th_hackathon_team2_android.login
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import com.example.kuit_4th_hackathon_team2_android.R
-import com.example.kuit_4th_hackathon_team2_android.databinding.ActivityHomeBinding
 import com.example.kuit_4th_hackathon_team2_android.databinding.ActivityLoginBinding
 import com.example.kuit_4th_hackathon_team2_android.home.HomeActivity
+import com.example.kuit_4th_hackathon_team2_android.signup.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -18,10 +16,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLogin.setOnClickListener{
+        binding.btnLoginSignIn.setOnClickListener{
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        binding.btnLoginSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
 
