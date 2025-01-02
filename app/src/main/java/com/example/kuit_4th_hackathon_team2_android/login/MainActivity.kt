a551d939988f1556fbb687376b43a66be82c285f
bw-green <bwgreen621@gmail.com>
2025-01-02 21:11:04 +0900

    login, 회원가입 예외처리

--- app/src/main/java/com/example/kuit_4th_hackathon_team2_android/login/MainActivity.kt
+++ app/src/main/java/com/example/kuit_4th_hackathon_team2_android/login/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kuit_4th_hackathon_team2_android.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kuit_4th_hackathon_team2_android.databinding.ActivityLoginBinding
 import com.example.kuit_4th_hackathon_team2_android.home.HomeActivity
@@ -9,6 +10,7 @@ import com.example.kuit_4th_hackathon_team2_android.signup.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private var canLogin = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +18,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLoginSignIn.setOnClickListener{
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+        binding.btnLoginSignIn.setOnClickListener {
+            checkingAccount()
+            if (canLogin) {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            } else {
+                binding.tvLoginWarning.visibility = View.INVISIBLE
+            }
+
             finish()
         }
 
@@ -28,4 +36,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkingAccount() {
+
+        // 로그인 기능 구현해야함
+
+        canLogin = true
+
+    }
 }
--- app/src/main/java/com/example/kuit_4th_hackathon_team2_android/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/kuit_4th_hackathon_team2_android/signup/SignUpActivity.kt
@@ -1,6 +1,11 @@
 package com.example.kuit_4th_hackathon_team2_android.signup
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +15,8 @@ import com.example.kuit_4th_hackathon_team2_android.databinding.ActivitySignUpBi
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private var canSignup = false
+    private var isAdmin = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +24,83 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+
+        initCheckingPassword()
+        initCheckingCheckBox()
+
+        initCheckingSignUp()
+
+    }
+
+    private fun initCheckingSignUp() {
         binding.btnSignUpSignUp.setOnClickListener {
-            finish()
+            canSignup = true
+            if (binding.etSignUpId.text.toString().isEmpty()) {
+                canSignup = false
+                binding.tvSignUpIdWarning.text = "아이디를 입력하세요."
+                binding.tvSignUpIdWarning.visibility = View.VISIBLE
+            } else {
+                binding.tvSignUpIdWarning.visibility = View.INVISIBLE
+            }
+
+            if (binding.etSignUpPassword.text.toString() == binding.etSignUpPasswordCheck.text.toString()) {
+                binding.tvSignUpPasswordWarning.visibility = View.INVISIBLE
+            } else {
+                canSignup = false
+                binding.tvSignUpPasswordWarning.text = "입력하신 비밀번호가 일치하지 않습니다."
+                binding.tvSignUpPasswordWarning.visibility = View.VISIBLE
+            }
+            val inputText = binding.etSignUpPasswordCheck.text.toString()
+            if (inputText.isEmpty()) {
+                canSignup = false
+                binding.tvSignUpPasswordWarning.text = "비밀번호 확인란이 비어 있습니다."
+                binding.tvSignUpPasswordWarning.visibility = View.VISIBLE
+            }
+
+            if (!binding.cbSignUpUser.isChecked && !binding.cbSignUpAdmin.isChecked) {
+                canSignup = false
+                binding.tvSignUpAuthorityWarning.text = "계정 권한을 선택하세요."
+                binding.tvSignUpAuthorityWarning.visibility = View.VISIBLE
+            } else {
+                binding.tvSignUpAuthorityWarning.visibility = View.INVISIBLE
+                if (binding.cbSignUpUser.isChecked) {
+                    isAdmin = false
+                } else {
+                    isAdmin = true
+                }
+            }
+
+            if (canSignup) {
+                finish()
+            }
+
+        }
+    }
+
+    private fun initCheckingCheckBox() {
+        binding.cbSignUpAdmin.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.cbSignUpUser.isChecked = false
+            }
+        }
+
+        binding.cbSignUpUser.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.cbSignUpAdmin.isChecked = false
+            }
+        }
+    }
+
+    private fun initCheckingPassword() {
+        binding.etSignUpPasswordCheck.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val imm =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager // 키보드 닫기
+                imm.hideSoftInputFromWindow(binding.etSignUpPasswordCheck.windowToken, 0)
+                true
+            } else {
+                false
+            }
         }
     }
 }
