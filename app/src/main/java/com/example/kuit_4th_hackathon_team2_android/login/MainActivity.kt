a7d01ec04a83f67de0a58fc2e37f2c1c53c4c4c4
pyosm217 <sumin020217@gmail.com>
2025-01-04 05:47:55 +0900

    마이페이지 get 완성

--- app/src/main/java/com/example/kuit_4th_hackathon_team2_android/login/MainActivity.kt
+++ app/src/main/java/com/example/kuit_4th_hackathon_team2_android/login/MainActivity.kt
@@ -3,19 +3,27 @@ package com.example.kuit_4th_hackathon_team2_android.login
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.example.kuit_4th_hackathon_team2_android.Retrofit.RetrofitObject
+import com.example.kuit_4th_hackathon_team2_android.Retrofit.service.LoginService
 import com.example.kuit_4th_hackathon_team2_android.databinding.ActivityLoginBinding
 import com.example.kuit_4th_hackathon_team2_android.home.HomeActivity
+import com.example.kuit_4th_hackathon_team2_android.login.model.LoginData
+import com.example.kuit_4th_hackathon_team2_android.login.model.ResponseData
 import com.example.kuit_4th_hackathon_team2_android.retrofit.LoginRequestData
 import com.example.kuit_4th_hackathon_team2_android.signup.SignUpActivity
 import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
-    private var canLogin = true
+    private var canLogin = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,15 +32,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnLoginSignIn.setOnClickListener {
-//            checkingAccount()
+            checkingAccount()
             if (canLogin) {
                 val intent = Intent(this, HomeActivity::class.java)
                 startActivity(intent)
+                finish()
             } else {
                 binding.tvLoginWarning.visibility = View.INVISIBLE
             }
 
-            finish()
+
         }
 
         binding.btnLoginSignUp.setOnClickListener {
@@ -42,14 +51,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkingAccount() {
-//        val id = binding.etLoginId.text.toString()
-//        val password = binding.etLoginPassword.toString()
-//        val service = RetrofitObject.retrofit.create(LoginService::class.java)
-//        val call = service.login(LoginRequestData(id, password))
+        val id = binding.etLoginId.text.toString().toLong()
+        val password = binding.etLoginPassword.text.toString()
+        val service = RetrofitObject.retrofit.create(LoginService::class.java)
+        Log.e("login", id.toString()+password)
+        val call = service.postLoginData(LoginData(id,password))
+        call.enqueue(object : Callback<ResponseData> {
+            override fun onResponse(call: Call<ResponseData>, response: Response<ResponseData>) {
+                if (response.isSuccessful) {
+                    val reservationResponse = response.body()
+                    val cookie =response.headers().values("Set-Cookie")
+                    val regex = "(?<=JSESSIONID=)[^;]+".toRegex()
+                    val cookieResult = regex.find(cookie.toString())?.value.toString()
+                    Log.e("fetch failure", cookieResult)
+                    canLogin=true
+                } else {
+                    Log.e("server response fail", "Server response failed with code: ${response.toString()}")
+                    binding.tvLoginWarning.visibility=View.VISIBLE
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseData>, t: Throwable) {
+                if (t is java.net.UnknownHostException) {
+                    Log.e("NetworkError", "Unable to resolve host: ${t.message}")
+                } else {
+                    Log.e("fetch failure", "Failed to fetch reservation items: ${t.message}")
+                }
+                binding.tvLoginWarning.visibility=View.VISIBLE
+            }
+
+        }
+        )
 
 
-
-        canLogin = true
+        //canLogin = true
 
     }
 }
---
+++ app/src/main/java/com/example/kuit_4th_hackathon_team2_android/login/model/LoginData.kt
@@ -0,0 +1,7 @@
+package com.example.kuit_4th_hackathon_team2_android.login.model
+
+data class LoginData(
+    val studentNum: Long,
+    val password: String
+)
+
